e946c3bfb5ba5c7010772e784855c6614774fb53
rahulbal <rahulbal29@gmail.com>
2015-10-03 19:31:21 +0530

    This is the initial file structure to follow

---
+++ src/session.c
@@ -0,0 +1,15 @@
+/***********************************
+ *
+ * Session.c ver 0.1
+ *
+ * This function creates a private
+ * session for a client that pings
+ * the network.
+ *
+ * Author : Rahul Balakrishnan
+ *
+ * Date : 3-October-2015
+ *
+ **********************************/
+
+#include<stdio.h>
