0e360e09f444c1f39a4bf93c5195b9195ef873e2
rahulbal <rahulbal29@gmail.com>
2015-10-04 23:41:51 +0530

    changes made to connect packet format

--- src/server/init.c
+++ src/server/init.c
@@ -18,9 +18,12 @@
 #include<sys/mman.h>
 #include<fcntl.h>
 #include<time.h>
+#include<pthread.h>
 #include "../commons/packet.c"
 
 #define BUFF_SIZE 64
+
+extern int session( char *, int * ); 
 
 int main( int argc, char * arg[] )
 {
@@ -33,7 +36,14 @@ int main( int argc, char * arg[] )
     int fd;
 
     void * attrib;
-    char * b;
+
+    char * client_mutex;
+    char * req_read_data;
+
+    char client_id;
+    client_id = 0;
+
+    int client_encrypt;
 
     fd = shm_open( "global", O_CREAT | O_TRUNC | O_RDWR, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP );
 
@@ -60,18 +70,33 @@ int main( int argc, char * arg[] )
 
     }
 
-    b = ( char * )attrib;
+    req_read_data = ( char * )attrib + 1;
 
-    while( *b != '*' ){
+    while(1){
 
-         usleep(1);
+         while( *req_read_data != '*' ){
+
+              usleep(1);
+
+         }
+
+         attrib = ( void * )( ( char * )attrib + 2 );
+         client_encrypt = *( int * )attrib;
+
+         if( client_id == 255 ){
+
+              fprintf( stderr, "unable to service" );
+
+         }
+
+         else( session( &client_id, &client_encrypt ) == 0 ){
+
+              fprintf( stderr, "Unable to allocate memory!" );
+
+         }
+
+         
 
     }
-
-    attrib = ( void * )( ++( char * )attrib );
-    client_id = *( char * )attrib;
-
-    attrib = ( void * )( ++( char * )attrib );
-    client_encrypt = *( int * )attrib;
 
 }
